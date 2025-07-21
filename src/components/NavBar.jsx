1abac13ce85c24cef01800768ec3be6129af8462
diaa <peachdiaa@gmail.com>
2025-07-21 22:22:41 +0300

    better navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./NavBar.css";
 import { CartContext } from "../contexts/CartContext";
 import { FavouritesContext } from "../contexts/FavouritesContext";
@@ -6,8 +6,42 @@ import { FavouritesContext } from "../contexts/FavouritesContext";
 const NavBar = () => {
   const { cart } = useContext(CartContext);
   const { favourites } = useContext(FavouritesContext);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const firstPage = document.querySelector("#first-page");
+    const selectGender = document.querySelector("#gender-sec");
+
+    if (!firstPage || !selectGender) return;
+
+    let isFirstVisible = true;
+    let isGenderVisible = true;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.target === firstPage) {
+            isFirstVisible = entry.isIntersecting;
+          }
+          if (entry.target === selectGender) {
+            isGenderVisible = entry.isIntersecting;
+          }
+          setScrolled(!isFirstVisible && !isGenderVisible);
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+    observer.observe(firstPage);
+    observer.observe(selectGender);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
   return (
-    <nav>
+    <nav className={scrolled ? "scrolled" : ""}>
       <div className="left-part">
         <h1>LAVA</h1>
       </div>
