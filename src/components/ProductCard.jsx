09a81ff59714443aa7804d91c936dde528018517
diaa <peachdiaa@gmail.com>
2025-07-24 14:53:41 +0300

    favs button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import "./ProductCard.css";
 import { useCart } from "../contexts/CartContext";
+import { useFavourites } from "../contexts/FavouritesContext";
 
 const ProductCard = ({ product }) => {
   const { toggleCart, cart } = useCart();
-  const isInCart = cart.some((item) => item.id === product.id);
+  const { toggleFavourite, favourites } = useFavourites();
 
-  const handleClick = () => {
-    toggleCart(product);
-  };
+  const isInCart = cart.some((item) => item.id === product.id);
+  const isFavourite = favourites.some((item) => item.id === product.id);
 
   return (
     <div className="product-card">
-      <img src={product.image_url} alt={product.name} />
+      <div className="product-header">
+        <img src={product.image_url} alt={product.name} />
+        <button
+          className={`favourite-btn ${isFavourite ? "active" : ""}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleFavourite(product);
+          }}
+          aria-label={
+            isFavourite ? "Remove from favourites" : "Add to favourites"
+          }
+        >
+          ♥
+        </button>
+      </div>
+
       <div className="lower-part">
         <div className="product-info">
           <h5>{product.name}</h5>
@@ -21,7 +36,10 @@ const ProductCard = ({ product }) => {
             <div className="price">{product.price}$</div>
           </div>
           <button
-            onClick={handleClick}
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleCart(product);
+            }}
             className={isInCart ? "remove-btn" : "add-btn"}
           >
             {isInCart ? "Remove from cart" : "Add to cart"}
